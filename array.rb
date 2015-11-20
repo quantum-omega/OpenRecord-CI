35329189b8ccae525c91814525a67b5e3aa506ba
François Labelle <quantum.omega@gmail.com>
2015-11-19 19:02:27 -0500

    Implémentation partielle d'Array.

--- array.rb
+++ array.rb
@@ -8,13 +8,15 @@ class Array
     DBC.check_type( id, Symbol,
                     "** L'argument a selectionner_avec doit etre un symbole" )
 
-    nil
+    self.select { |item| yield item.send id }
   end
 
   def method_missing( id, *args, &block )
     nom_methode = "#{id}"
-
-
-    raise NoMethodError, "Methode non-definie `#{id}' pour #{self}", caller(1)
+    if nom_methode.start_with?('selectionner_avec')
+    then
+      avec_quoi = nom_methode[18..-1].to_sym
+      return selectionner_avec avec_quoi block
+    end
   end
 end
